37016f75ebcfbaee235d124c67b002e82992decb
关于 <372301467@qq.com>
2021-12-10 16:06:37 +0800

    初始化 header

--- src/entry/index.jsx
+++ src/entry/index.jsx
@@ -2,5 +2,6 @@ import React from 'react'
 import ReactDom from 'react-dom'
 import App from './app'
 import 'animate.css'
+import './index.styl'
 
 ReactDom.render(<App />, document.getElementById('app-root'))
--- src/page/just-type/entry/head/index.jsx
+++ src/page/just-type/entry/head/index.jsx
@@ -1,5 +1,14 @@
 import React from 'react'
+import './index.styl'
+import ToolBar from './tool-bar'
 
 export default function() {
-  return <div>head</div>
+  return <div className = 'just-type-head'>
+    <div className = 'app-title'>Just Type</div>
+    <div className = 'head-border'>
+      <div className = 'left' />
+      <div className = 'left' />
+    </div>
+    <ToolBar />
+  </div>
 }
