561047ebf27178082c7471f715162a7119f3a24f
关于 <372301467@qq.com>
2021-12-10 17:40:09 +0800

    header 样式

--- src/page/just-type/entry/head/index.jsx
+++ src/page/just-type/entry/head/index.jsx
@@ -5,10 +5,6 @@ import ToolBar from './tool-bar'
 export default function() {
   return <div className = 'just-type-head'>
     <div className = 'app-title'>Just Type</div>
-    <div className = 'head-border'>
-      <div className = 'left' />
-      <div className = 'left' />
-    </div>
     <ToolBar />
   </div>
 }
