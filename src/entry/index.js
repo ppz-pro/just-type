f6eab453510e9f99ef4681c624726d63e094466b
gy <372301467@qq.com>
2021-12-29 15:04:43 +0800

    引入 iconfont

--- src/entry/index.js
+++ src/entry/index.js
@@ -4,5 +4,6 @@ import App from './app'
 import 'animate.css'
 import './index.styl'
 import '@ppzp/style'
+import './icon/iconfont.css'
 
 ReactDom.render(<App />, document.getElementById('app-root'))
--- src/page/just-type/entry/head/tool-bar.js
+++ src/page/just-type/entry/head/tool-bar.js
@@ -2,6 +2,8 @@ import React from 'react'
 
 export default function() {
   return <div className = 'tool-bar'>
-    <a className = 'top-btn' href = 'https://github.com/ppz-pro/just-type' title = '源码在这里'>github</a>
+    <a className = 'top-btn' href = 'https://github.com/ppz-pro/just-type' title = '源码在这里'>
+      <span className = 'iconfont icon-github'></span>
+    </a>
   </div>
 }
