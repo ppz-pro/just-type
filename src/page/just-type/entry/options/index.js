2d0ea98aed7dbfbe9db4cd5a78f25d335dd2268c
关于 <372301467@qq.com>
2021-12-31 18:41:06 +0800

    页面头部

--- src/page/just-type/entry/options/index.js
+++ src/page/just-type/entry/options/index.js
@@ -3,7 +3,15 @@ import './index.styl'
 
 export default function() {
   return <div className = 'jt-options'>
-  <button>Sign in</button>
-  <a href = 'https://github.com/ppz-pro/just-type' className = 'iconfont icon-github'></a>
+    <div>
+      <span>当前词库:</span>
+      <span></span>
+    </div>
+    <div className = 'right'>
+      <button>我的词库</button>
+      <button>表白墙</button>
+      <button>登录</button>
+      <a href = 'https://github.com/ppz-pro/just-type' className = 'iconfont icon-github'></a>
+    </div>
   </div>
 }
